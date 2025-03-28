70c757ffc69bf4787e567957125bbc4262341d38
kiisi <kiisifelix06@gmail.com>
2025-03-28 23:24:15 +0100

    fix: next middleware redirect

--- middleware.ts
+++ middleware.ts
@@ -34,7 +34,7 @@ export async function middleware(request: NextRequest) {
         console.log(error)
         const url = request.nextUrl.clone();
         url.pathname = "/login";
-        return NextResponse.redirect(url);
+        return NextResponse.redirect(new URL(url));
     }
 }
 
