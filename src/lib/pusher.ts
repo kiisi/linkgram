b984db340706a56ebc9796f014f8ef56ea338255
kiisi <kiisifelix06@gmail.com>
2025-03-29 02:17:51 +0100

    optimise pusher

--- src/lib/pusher.ts
+++ src/lib/pusher.ts
@@ -2,4 +2,6 @@ import Pusher from "pusher-js";
 
 export const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_APP_KEY!, {
     cluster: process.env.NEXT_PUBLIC_PUSHER_APP_CLUSTER!,
+    forceTLS: true,
+    enabledTransports: ['ws', 'wss'],
 });
